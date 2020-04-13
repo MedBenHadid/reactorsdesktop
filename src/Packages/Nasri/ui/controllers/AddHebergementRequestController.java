bed42338b2dc8f2594d8c189a88adeb196b0db77
Mohamed Nasri <muhammed.al.nasri@gmail.com>
2020-04-13 13:27:18 +0100

    add input validation for requests

--- src/Packages/Nasri/ui/controllers/AddHebergementRequestController.java
+++ src/Packages/Nasri/ui/controllers/AddHebergementRequestController.java
@@ -7,10 +7,7 @@ import Packages.Nasri.services.ServiceHebergementRequest;
 import Packages.Nasri.utils.Helpers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -42,6 +39,14 @@ public class AddHebergementRequestController implements Initializable {
 
     @FXML
     protected void addButton() {
+        System.out.println(arrivalDateInput.getValue());
+//        return;
+
+        if (!inputIsValid()) {
+            return;
+        }
+
+
         HebergementRequest hebergementRequest = new HebergementRequest();
 
         // 72 is a temporary value of user_id
@@ -68,6 +73,76 @@ public class AddHebergementRequestController implements Initializable {
         ServiceHebergementRequest serviceHebergementRequest = new ServiceHebergementRequest();
         serviceHebergementRequest.add(hebergementRequest);
         close();
+    }
+
+    private boolean inputIsValid() {
+        // checking if the name field is empty
+        if (this.nameInput.getText().trim().isEmpty()) {
+            //display error dialog
+            displayErrorDialog("Champ de saisie 'Nom de demandeur' est vide");
+            return false;
+        }
+
+        // checking if the description field is empty
+        if (this.descriptionInput.getText().trim().isEmpty()) {
+            //display error dialog
+            displayErrorDialog("Champ de saisie 'Description' est vide");
+            return false;
+        }
+
+        // checking if the region field is empty
+        if (this.regionInput.getText().trim().isEmpty()) {
+            //display error dialog
+            displayErrorDialog("Champ de saisie 'Region' est vide");
+            return false;
+        }
+
+        // checking if the nativeCountry field is empty
+        if (this.nativeCountryInput.getText().trim().isEmpty()) {
+            displayErrorDialog("Champ de saisie 'Pays d'origine' est vide");
+            return false;
+        }
+
+        // checking if the passport field is empty
+        if (this.passportInput.getText().trim().isEmpty()) {
+            displayErrorDialog("Champ de saisie 'Numero de passport' est vide");
+            return false;
+        }
+
+        // checking if the numberRooms field is < 1 || not numeric
+        try {
+            int numberChildren = Integer.parseInt(numberChildrenInput.getText());
+            if (numberChildren < 1) {
+                displayErrorDialog("Valeure inferieure ou égale a 0 pour le champ de saisie 'Nombre d'enfants'");
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            displayErrorDialog("Valeure incorrect pour le champ de saisie 'Nombre d'enfants'");
+            return false;
+        }
+
+        // checking if the arrivalDate field is empty
+        if (this.arrivalDateInput.getValue() == null) {
+            displayErrorDialog("Champ de saisie 'Date d'arrivée' est vide");
+            return false;
+        }
+
+        // checking if the phoneNUmber field is empty || incorrect
+        if (this.telephoneInput.getText().trim().isEmpty()) {
+            displayErrorDialog("Champ de saisie 'Téléphone' est vide");
+            return false;
+        } else if (!Helpers.phoneNumberIsValid(this.telephoneInput.getText())) {
+            displayErrorDialog("Valeure incorrect pour le champ de saisie 'Téléphone'");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void displayErrorDialog(String errorMessage) {
+        Alert alert =
+                new Alert(Alert.AlertType.ERROR, errorMessage);
+        alert.showAndWait();
     }
 
     private void close() {
