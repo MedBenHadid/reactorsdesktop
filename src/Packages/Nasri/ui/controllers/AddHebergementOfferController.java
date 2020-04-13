29a2b7ca096c873e78c80742f38eac7186138d82
Mohamed Nasri <muhammed.al.nasri@gmail.com>
2020-04-13 13:02:20 +0100

    add input verification for offers

--- src/Packages/Nasri/ui/controllers/AddHebergementOfferController.java
+++ src/Packages/Nasri/ui/controllers/AddHebergementOfferController.java
@@ -3,8 +3,11 @@ package Packages.Nasri.ui.controllers;
 import Packages.Nasri.entities.HebergementOffer;
 import Packages.Nasri.enums.HebergementStatus;
 import Packages.Nasri.services.ServiceHebergementOffer;
+import Packages.Nasri.utils.Helpers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
@@ -13,6 +16,7 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AddHebergementOfferController implements Initializable {
@@ -28,7 +32,7 @@ public class AddHebergementOfferController implements Initializable {
     TextField telephoneInput;
 
     FileChooser imageFileChooser = new FileChooser();
-    String imageFilePath;
+    String imageFilePath = "";
 
     @FXML
     protected void brownForPictureButton() {
@@ -45,6 +49,11 @@ public class AddHebergementOfferController implements Initializable {
     }
     @FXML
     protected void addButton() {
+
+        if (!inputIsValid()) {
+            return;
+        }
+
         HebergementOffer hebergementOffer = new HebergementOffer();
 
         // 72 is a temporary value of user_id
@@ -63,6 +72,68 @@ public class AddHebergementOfferController implements Initializable {
         ServiceHebergementOffer serviceHebergementOffer = new ServiceHebergementOffer();
         serviceHebergementOffer.add(hebergementOffer);
         close();
+    }
+
+    private boolean inputIsValid() {
+        // checking if the description field is empty
+        if (this.descriptionInput.getText().trim().isEmpty()) {
+            //display error dialog
+            displayErrorDialog("Champ de saisie 'Description' est vide");
+            return false;
+        }
+
+        // checking if the numberRooms field is < 1 || not numeric
+        try {
+            int numberRooms = Integer.parseInt(numberRoomsInput.getText());
+            if (numberRooms < 1) {
+                displayErrorDialog("Valeure inferieure ou égale a 0 pour le champ de saisie 'Nombre de chambres'");
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            displayErrorDialog("Valeure incorrect pour le champ de saisie 'Nombre de chambres'");
+            return false;
+        }
+
+        // checking if the governorat field is empty
+        if (this.governoratInput.getText().trim().isEmpty()) {
+            displayErrorDialog("Champ de saisie 'Governorat' est vide");
+            return false;
+        }
+
+        // checking if the durationInput field is < 1 || not numeric
+        try {
+            int duration = Integer.parseInt(durationInput.getText());
+            if (duration < 1) {
+                displayErrorDialog("Valeure inferieure ou égale a 0 pour le champ de saisie 'Durée (mois)'");
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            displayErrorDialog("Valeure incorrect pour le champ de saisie 'Durée (mois)'");
+            return false;
+        }
+
+        // checking if the phoneNUmber field is empty || incorrect
+        if (this.telephoneInput.getText().trim().isEmpty()) {
+            displayErrorDialog("Champ de saisie 'Téléphone' est vide");
+            return false;
+        } else if (!Helpers.phoneNumberIsValid(this.telephoneInput.getText())) {
+            displayErrorDialog("Valeure incorrect pour le champ de saisie 'Téléphone'");
+            return false;
+        }
+
+        // checking if the image is selected
+        if (this.imageFilePath.isEmpty()) {
+            displayErrorDialog("Aucune image selectionée");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void displayErrorDialog(String errorMessage) {
+        Alert alert =
+                new Alert(Alert.AlertType.ERROR, errorMessage);
+        alert.showAndWait();
     }
 
     private void close() {
--- src/Packages/Nasri/utils/Helpers.java
+++ src/Packages/Nasri/utils/Helpers.java
@@ -5,6 +5,7 @@ import Packages.Nasri.enums.HebergementStatus;
 
 import java.sql.Date;
 import java.time.LocalDateTime;
+import java.util.regex.Pattern;
 
 public class Helpers {
     public static Date convertLocalDateTimeToDate(LocalDateTime localDateTime) {
@@ -21,5 +22,17 @@ public class Helpers {
 
     public static String convertCivilStateToFrench(String civilStage) {
         return civilStage.equals(CivilStatus.Married.name()) ? "Marié(e)" : "Célibataire";
+    }
+
+    public static boolean phoneNumberIsValid(String phoneNumber) {
+        //source: https://stackoverflow.com/questions/42104546/java-regular-expressions-to-validate-phone-numbers
+
+        //\d{8} matches 12345678
+
+        String regex = "\\d{8}";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        return pattern.matcher(phoneNumber).matches();
     }
 }
